9043ba167dd567bd7b23e7c73e346bc35b7dca1e
sandalu <sandaluthushan20@gmail.com>
2026-09-02 13:28:02 +0530

    Upgrade Next.js security patch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={`${interfaceFont.variable} ${monoFont.variable}`}>{children}</body>
     </html>
   );
